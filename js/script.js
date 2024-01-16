ef75b1eae3ba159c180afe226caf58fcb851b0b4
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-16 14:54:08 +0100

    snack 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 console.log('JS ok');
 
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
 
 
+const guestList = [];
+let guestobject = {};
+
+
+const list = guests.forEach((guest , i) => {
+    
+
+   
+    guestobject += 
+  `
+        tableName : ${tableName},
+        name : ${guest},
+        place : ${i}
+  `;
+
+  guestList.push(guestobject);
+
+});
+
+console.log(guestList);
