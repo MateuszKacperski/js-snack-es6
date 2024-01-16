35a0ee33eb5795a5688b70c77a25d5325f28a789
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-16 15:18:06 +0100

    snack 2

--- js/script.js
+++ js/script.js
@@ -36,3 +36,69 @@ const list = guests.forEach((guest , i) => {
 });
 
 console.log(guestList);
+
+
+
+
+
+
+//snack 2 
+
+
+//creo la lista degli studenti 
+const totalStudents = [
+    {
+        id : 213,
+        name : 'Marco della Rovere',
+        grades : 78
+    },
+    {
+        id : 110,
+        name : 'Paola Cortellessa',
+        grades : 96
+    },
+    {
+        id : 250,
+        name : 'Andrea Mantegna',
+        grades : 48
+    },
+    {
+        id : 145,
+        name : 'Gaia Borromini',
+        grades : 74
+    },
+    {
+        id : 196,
+        name : 'Luigi Grimaldello',
+        grades : 68
+    },
+    {
+        id : 102,
+        name : 'Piero della Francesca',
+        grades : 50
+    },
+    {
+        id : 120,
+        name : 'Francesca da Polenta',
+        grades : 84
+    }
+]
+
+//studenti totale voti superiore a 70
+const students70 = totalStudents.filter(({grades}) => grades >= 70);
+
+console.log(students70);
+
+// lista studenti totale voti superiore a 70 e id supeirore a 120
+
+const students120 = totalStudents.filter(({grades , id}) => grades > 70 && id > 120);
+
+console.log(students120);
+
+const studentsUpper = totalStudents.filter(student => {
+    const studente = student.name;
+    return studente.toUpperCase();
+});
+
+
+console.log(studentsUpper);
